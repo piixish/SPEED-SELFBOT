f13cb837f033a2c816af96a806421e81081feb24
mizuki <148379678+piixish@users.noreply.github.com>
2024-04-21 21:31:57 +0200

    Delete speed-no-grab-main directory

--- speed-no-grab-main/commands/Tools/closedms.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "closedms",
-    description: "Close all your dms",
-    run: async (client, message, args) => {
-        message.edit("> **SPEED**")
-        message.delete().catch(() => false)
-        client.channels.cache.filter((channel) => channel.type === "DM" | channel.type === "GROUP_DM").map((channel) => channel.delete().catch(() => false))
-    }
-}
